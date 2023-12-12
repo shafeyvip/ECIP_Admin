75e6fdf65f342ab11b2f5a6752bb838b7e44e582
Ahmed Shafey <shafeyvip@gmail.com>
2023-12-12 14:19:50 +0200

    first commit

---
+++ Tutor.md
@@ -0,0 +1,26 @@
+- frontend template
+- virtualenv:
+  - creat
+  - activate [widows : source ./scripts/activate]
+  - pip install
+  - deactivate
+
+- upload progect on github
+
+- url : path
+- view : logic
+- models : db
+- templates : frontend
+
+
+
+
+Relations :
+    - One-to-Many  [Auther - Posts]          Foreginkey
+    - Many-to-Many [Admin_User - Groups]       
+    - One-to-One   [User - Profile]
+
+
+static files : [frontend] images, css, javascript
+
+media files : [upload] images
---
+++ device/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+# Create your models here.
+DEVICE_TYPE = (
+    ('PC','PC'),
+    ('Laptop','Laptop'),
+    ('Monitor','Monitor'),
+    ('Printer','Printer'),
+)
+
+DEVICE_STATUS = (
+    ('New','New'),
+    ('Used','Used'),
+    ('Damaged','Damaged'),
+)
+class device(models.Model):
+    device_code = models.IntegerField(auto_created=True,default=7040000)
+    device_type = models.CharField(max_length=15, choices=DEVICE_TYPE)
+    model_name = models.CharField(max_length=50)
+    motherboard = models.CharField(max_length=50)
+    cpu = models.CharField(max_length=50)
+    ram = models.CharField(max_length=50)
+    hdd = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, choices=DEVICE_STATUS)
+    sites = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    other = models.TextField(max_length=255)
+
+    def __str__(self):
+        return self.model_name
